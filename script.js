2c04400d3476cbffb254a40453c12a14c69e0ee8
swhoxf <108375735+swhoxf@users.noreply.github.com>
2023-01-21 16:39:21 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// run initial animations 
+function init() {
+  // const cards = document.getElementsByClassName('project-card')
+  // for (let i=0; i<cards.length; i++) {
+  //   cards[i].classList.add('tilt-in-top-1')
+  // }
+  // setTimeout(() => {
+  //   for (let i = 0; i<cards.length; i++) {
+  //     cards[i].classList.remove('tilt-in-top-1')
+  //   }
+  // }, 2000)
+  // for (let i=0; i<cards.length; i++) {
+  //   cards[i].addEventListener('mouseenter', () => cards[i].classList.add('jello-horizontal'))
+  // }
+}
